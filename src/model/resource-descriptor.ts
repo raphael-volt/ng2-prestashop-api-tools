693bfb25b9db362f1b4515b76e4097895e6ebfef
raphael_volt <raphael.ketmie@gmail.com>
2017-06-23 04:25:30 +0200

    added model interfaces

---
+++ src/model/resource-descriptor.ts
@@ -0,0 +1,46 @@
+
+export interface ResourceDescriptorCollection {
+    [name: string]: ResourceDescriptor
+}
+
+export interface ResourceDescriptor {
+    get: boolean
+    post: boolean
+    put: boolean
+    delete: boolean
+}
+
+export interface PropertyValidator {
+    method: string
+    maxSize: boolean
+}
+
+export interface PropertyDescriptorCollection {
+    [name: string]: PropertyDescriptor
+}
+
+export interface PropertyDescriptor {
+    translatable: boolean
+    required: boolean
+    readOnly: boolean
+    validator: PropertyValidator
+}
+
+export interface AssociationResource {
+    [name: string]: AssociationNode
+}
+
+export interface AssociationNode {
+    [name: string]: AssociationNodeProperty
+}
+
+export interface AssociationNodeProperty {
+    [name: string]: boolean
+}
+
+export interface ResourceSynopsis {
+    nodeName: string
+    resource: string
+    properties: PropertyDescriptorCollection
+    associations?: AssociationResource
+}
